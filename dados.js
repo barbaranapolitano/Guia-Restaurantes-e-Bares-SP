7186e8eecac067102e49e84c57f7480374e7c66f
Barbara Napolitano <banapolitano@hotmail.com>
2024-09-05 21:37:02 -0300

    joao

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados =  [
+    {
+        titulo : "Onde comer em SP?",
+        descrição : "São Paulo é um verdadeiro paraíso para os amantes da boa comida. A cidade concentra uma das gastronomias mais diversificadas do mundo, com influências de diversas culturas e cozinhas. Desde os tradicionais pratos brasileiros, como a feijoada e a coxinha, até as mais sofisticadas criações da alta gastronomia, passando por uma infinidade de opções internacionais, a capital paulista oferece uma experiência gastronômica única a cada esquina. ",
+        link : "https://vejasp.abril.com.br/coluna/arnaldo-lorencato/comer-e-beber-2023-melhores-restaurantes"
+
+    },
+    {
+        titulo : "Alta gastronomia em São Paulo (Guia Michelin)",
+        descrição : "Os restaurantes agraciados com estrelas Michelin representam o ápice da gastronomia mundial. Em São Paulo, essa distinção celebra a criatividade, a técnica impecável e a seleção rigorosa de ingredientes de alta qualidade. Os chefs destes estabelecimentos oferecem experiências culinárias únicas, com pratos que são verdadeiras obras de arte, combinando sabores exóticos e clássicos de forma harmoniosa. A alta gastronomia Michelin é um convite para uma jornada sensorial, onde cada mordida é uma descoberta e um deleite para o paladar." ,
+        link : "https://guide.michelin.com/br/pt_BR/sao-paulo-region/sao-paulo/restaurants" 
+    },
+    {
+        titulo : "Melhores bares de coquetelaria em SP", 
+        descrição : "A cena de coquetelaria em São Paulo é vibrante e em constante evolução, refletindo a diversidade cultural da cidade. Dos clássicos revisitados aos drinks autorais mais inovadores, a capital paulista oferece uma experiência única para os amantes de coquetéis." ,
+        link : "https://[link para um guia de bares de coquetelaria em SP]",
+    }
+];
+
+    
+    
+        
